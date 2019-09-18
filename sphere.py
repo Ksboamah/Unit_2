8dad0d0452caac7ed161bce84ac8c7c8af4ca60c
Kwame <bkwamesakyi@gmail.com>
2019-09-18 14:40:19 -0400

    I imported turtle. I began the class assesment.

--- sphere.py
+++ sphere.py
@@ -1,1 +1,21 @@
-import turtle
+volume_of_sphere = 4 / 3 * 3.14 * 5**3
+
+print("The volume of a sphere is",volume_of_sphere)
+num_books = 60
+book_cost = 24.96
+bookstore_price_of_book = ((60 / 100) * book_cost) * num_books
+cost_of_shipping_60_books = 3 + (0.75 * (num_books - 1))
+
+print("The wholesale price of the books is",bookstore_price_of_book + cost_of_shipping_60_books )
+
+departure_time = 6.52
+easy_pace = 8.15 * 60
+easy_pace_miles = 2
+tempo_pace = 7.12 * 60
+tempo_pace_miles = 3
+run_time_secs= (easy_pace * easy_pace_miles) + (tempo_pace * tempo_pace_miles)
+run_time = (run_time_secs / 60)
+arrival_time = runtime
+
+print(run_time)
+
