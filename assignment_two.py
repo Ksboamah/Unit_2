172ff8e18ac7c8cb76a1c3ac00b34016d41662dc
Kwame <bkwamesakyi@gmail.com>
2019-09-23 10:15:13 -0400

    I added my Unit 2 flowchart. I created the assignment_two file. I began typing the code. I only have a couple of line.

--- assignment_two.py
+++ assignment_two.py
@@ -1,4 +1,5 @@
 # By Kwame Boamah on Monday, September 23, 2019
 
-user_name = str(input("Hello. What is your name"))
-print(user_name)
+user_name = str(input("Hello. What is your name?"))
+print("Hello", user_name, "my name is Randy.")
+user_location = str(input("So, where are you from?"))
